016406114442a4bfb7d7139587e8e15e4331dc1f
LooreDev <diegolorenzo.m02@gmail.com>
2024-03-02 22:09:14 -0600

    Validaciones de campos en el archivo de registro de usuarios

--- register/register-student.php
+++ register/register-student.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once '../conexion.php';
 
 // Procesar datos del formulario
@@ -8,6 +7,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $contrasenia = $_POST["contrasenia"];
 
+    // Verificar si la matrícula ya existe en la base de datos
+    $sql_check = "SELECT matricula FROM alumnos WHERE matricula = '$matricula'";
+    $result_check = $conn->query($sql_check);
+
+    if ($result_check->num_rows > 0) {
+        echo "La matrícula ya está registrada en la base de datos.";
+        exit(); // Salir del script
+    }
+
+    // La matrícula no está duplicada, proceder con la inserción
     $passencrypt = password_hash($contrasenia, PASSWORD_DEFAULT);
 
     // Insertar datos en la tabla de usuarios
